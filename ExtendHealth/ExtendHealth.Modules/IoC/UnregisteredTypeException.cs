44f3cd616802a59af3ade4f91e12bda6d49b9542
Alex Abenoja <abenojaalex@gmail.com>
2013-06-26 17:46:12 -0600

    Added a new exception type to be used by the container

---
+++ ExtendHealth/ExtendHealth.Modules/IoC/UnregisteredTypeException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendHealth.Modules.IoC
+{
+    public class UnregisteredTypeException : Exception
+    {
+        public UnregisteredTypeException() : base("The requested type has not been registered in the container.") { }
+    }
+}
